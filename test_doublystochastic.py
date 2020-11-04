a1d34eaeee042d07101995826686598d9dc18d1c
Satyadev Ntv <tvsatyadev@gmail.com>
2020-11-04 13:46:28 +0530

    GPU computations.

--- test_doublystochastic.py
+++ test_doublystochastic.py
@@ -9,11 +9,13 @@ from pymanopt import function
 
 from IPython import embed
 
+import cupy as cp
+
 def test_doublystochastic():
     rnd.seed(21)
 
-    ns = [100, 100] * 10
-    ms = [200, 200] * 10
+    ns = [100] * 1000
+    ms = [200] * 1000
     batch = len(ns)
 
     p = []
@@ -27,12 +29,12 @@ def test_doublystochastic():
         q.append(q0 / np.sum(q0))
         A0 = rnd.rand(n, m)
         A0 = A0[np.newaxis, :]
-        A0 = SKnopp(A0, p[i], q[i], n+m)
+        A0 = SKnopp(A0, p[i], q[i], n+m).get()
         A.append(A0)
     A = np.vstack((C for C in A))
 
     def _cost(x):
-        return 0.5 * (np.linalg.norm(x - A)**2)
+        return 0.5 * (cp.linalg.norm(cp.array(x) - cp.array(A))**2)
 
     def _egrad(x):
         return x - A
@@ -41,18 +43,24 @@ def test_doublystochastic():
         return u
 
     manf = DoublyStochastic(n, m, p, q)
-    solver = TrustRegions(maxiter=50, maxtime=100000)
+    solver = ConjugateGradient(maxiter=50, maxtime=100000)
     prblm = Problem(manifold=manf, cost=lambda x: _cost(x), egrad=lambda x: _egrad(x), ehess=lambda x, u: _ehess(x, u), verbosity=3)
 
     U = manf.rand()
     Uopt = solver.solve(prblm, x=U)
-    for i in range(batch):
-        print(f"""
-    For {i}
-    Row constraint err: {np.linalg.norm(np.sum(Uopt[i], axis=0) - q[i])}
-    Col constraint err: {np.linalg.norm(np.sum(Uopt[i], axis=1) - p[i])}
+    print(f"""
+For all
+Row constraint err: {np.linalg.norm(np.sum(Uopt, axis=1) - q)}
+Col constraint err: {np.linalg.norm(np.sum(Uopt, axis=2) - p)}
 
-        """)
+    """)
+    #for i in range(batch):
+    #    print(f"""
+    #For {i}
+    #Row constraint err: {np.linalg.norm(np.sum(Uopt[i], axis=0) - q[i])}
+    #Col constraint err: {np.linalg.norm(np.sum(Uopt[i], axis=1) - p[i])}
+
+    #    """)
 
 
 if __name__ == "__main__":
